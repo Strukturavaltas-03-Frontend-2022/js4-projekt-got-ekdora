0e8beb459e44fac51ca602ebff8fad20f47c17f9
ekdora <dora.egrykiss@gmail.com>
2022-09-10 18:56:01 +0200

    Mobile layout

--- js/got.js
+++ js/got.js
@@ -2,13 +2,27 @@ const got = document.querySelector('.got__map');
 const sidebar = document.querySelector('.got__sidebar');
 const charInfoPlaceholder = document.querySelector('.got__char-info_placeholder');
 const searchDiv = document.querySelector('.got__search');
+
+const modal = document.querySelector('.got__modal');
+const closeModalX = document.querySelector('.modal__close-btn');
+const modalCharInfoPlaceholder = document.querySelector('.modal__char-info_placeholder');
+const modalSearchDiv = document.querySelector('.modal__search');
+
 let profiles = [];
-let searchHasError = false;
 
 const createProfile = (detail) => {
     const profile = document.createElement('div');
     profile.classList.add('got__pic', 'flex', 'flex-col', 'items-center');
-    profile.onclick = () => selectProfile(detail, profile);
+    profile.id = detail.name.replace(' ', '');
+    profile.onclick = () => {
+        if (window.innerWidth < 600) {
+            modal.classList.remove("hidden-transition");
+            modal.classList.add("visible-transition");
+            closeModalX.focus();
+        }
+        selectProfile(detail, profile);
+        showSearchInputError(false);
+    }
 
     const profileImg = document.createElement('img');
     profileImg.classList.add('img-size');
@@ -36,7 +50,7 @@ const createCharInfo = (detail) => {
     charInfo.appendChild(charImg);
 
     const charShortInfo = document.createElement('div');
-    charShortInfo.classList.add('flex', 'items-center', 'justify-between', 'w-full');
+    charShortInfo.classList.add('char-short', 'flex', 'items-center', 'justify-between', 'w-full');
     charInfo.appendChild(charShortInfo);
 
     const charName = document.createElement('div');
@@ -55,6 +69,7 @@ const createCharInfo = (detail) => {
     charInfo.appendChild(charBio);
 
     charInfoPlaceholder.appendChild(charInfo);
+    modalCharInfoPlaceholder.appendChild(charInfo.cloneNode(true));
 };
 
 const selectProfile = (detail, profileHtmlEl) => {
@@ -68,53 +83,58 @@ const selectProfile = (detail, profileHtmlEl) => {
 };
 
 const deleteCharInfo = () => {
-    const charInfo = document.querySelector('.got__char-info');
-    if (charInfo) {
-        charInfo.remove();
+    const charInfoArr = document.querySelectorAll('.got__char-info');
+    if (charInfoArr.length) {
+        charInfoArr[0].remove();
+        charInfoArr[1].remove();
     }
 };
 
 (function createSearch() {
-    const searchInputDiv = document.createElement('div');
-    searchInputDiv.classList.add('flex', 'items-center', 'justify-evenly');
+    const htmlObj = {};
+    /* Ez ezért kell, mert az eventListener-eket nem clone-ozza a cloneNode */
+    for (let i = 1; i < 3; i++) {
+        const htmlObjSearchInputDiv = `searchInputDiv${i}`;
+        htmlObj[htmlObjSearchInputDiv] = document.createElement('div');
+        htmlObj[htmlObjSearchInputDiv].classList.add('flex', 'items-center', 'justify-evenly');
 
-    const searchInput = document.createElement('input');
-    searchInput.classList.add('search-input');
-    searchInput.type = 'text';
-    searchInput.placeholder = 'Search a character';
-    searchInput.onfocus = () => showSearchInputError(false);
+        const htmlObjSearchInput = `searchInput${i}`;
+        htmlObj[htmlObjSearchInput] = document.createElement('input');
+        htmlObj[htmlObjSearchInput].classList.add('search-input');
+        htmlObj[htmlObjSearchInput].type = 'text';
+        htmlObj[htmlObjSearchInput].placeholder = 'Search a character';
+        htmlObj[htmlObjSearchInput].onfocus = () => showSearchInputError(false);
 
-    const searchBtn = document.createElement('button');
-    searchBtn.classList.add('search-btn');
-    searchBtn.innerHTML = '❯';
-    searchBtn.onclick = () => {
-        if (searchInput.value) {
-            findProfile(searchInput.value);
-            searchInput.value = '';
+        const htmlObjSearchBtn = `searchBtn${i}`;
+        htmlObj[htmlObjSearchBtn] = document.createElement('button');
+        htmlObj[htmlObjSearchBtn].classList.add('search-btn');
+        htmlObj[htmlObjSearchBtn].innerHTML = '❯';
+        htmlObj[htmlObjSearchBtn].onclick = () => {
+            if (htmlObj[htmlObjSearchInput].value) {
+                findProfile(htmlObj[htmlObjSearchInput].value);
+                htmlObj[htmlObjSearchInput].value = '';
+            }
         }
+
+        const htmlObjErrorDiv = `errorDiv${i}`;
+        htmlObj[htmlObjErrorDiv] = document.createElement('div');
+        htmlObj[htmlObjErrorDiv].classList.add('search-error', 'flex', 'items-center', 'justify-center');
+        htmlObj[htmlObjErrorDiv].innerHTML = 'Character not found';
+
+        htmlObj[htmlObjSearchInputDiv].appendChild(htmlObj[htmlObjSearchBtn]);
+        htmlObj[htmlObjSearchInputDiv].appendChild(htmlObj[htmlObjSearchInput]);
     }
 
-    const errorDiv = document.createElement('div');
-    errorDiv.classList.add('search-error', 'flex', 'items-center', 'justify-center');
-    errorDiv.innerHTML = 'Character not found';
-
-    searchInputDiv.appendChild(searchBtn);
-    searchInputDiv.appendChild(searchInput);
-    searchDiv.appendChild(searchInputDiv);
-    searchDiv.appendChild(errorDiv);
+    searchDiv.appendChild(htmlObj['searchInputDiv1']);
+    searchDiv.appendChild(htmlObj['errorDiv1']);
+    modalSearchDiv.appendChild(htmlObj['searchInputDiv2']);
+    modalSearchDiv.appendChild(htmlObj['errorDiv2']);
 })();
 
 const findProfile = (searchStr) => {
     const searchedProfile = profiles.filter(profile => profile.name.toLowerCase() === searchStr.toLowerCase());
     if (searchedProfile.length) {
-        const profileHtmlEls = document.querySelectorAll('.got__pic');
-        let isFound = false;
-        let activatedProfile;
-        for (let i = 0; i < profileHtmlEls.length && !isFound; i++) {
-            activatedProfile = profileHtmlEls[i];
-            isFound = activatedProfile.querySelector('.img-name').innerHTML.toLowerCase() === searchStr.toLowerCase();
-        };
-
+        const activatedProfile = document.querySelector(`#${searchedProfile[0].name.replace(' ', '')}`);
         selectProfile(searchedProfile[0], activatedProfile);
     } else {
         showSearchInputError();
@@ -122,10 +142,17 @@ const findProfile = (searchStr) => {
 };
 
 const showSearchInputError = (show = true) => {
-    const errorDiv = document.querySelector('.search-error');
-    show ? errorDiv.classList.add('search-error--on') : errorDiv.classList.remove('search-error--on');
-    
-}
+    const errorDivArr = document.querySelectorAll('.search-error');
+    show
+        ? (() => {
+            errorDivArr[0].classList.add('search-error--on');
+            errorDivArr[1].classList.add('search-error--on');
+        })()
+        : (() => {
+            errorDivArr[0].classList.remove('search-error--on');
+            errorDivArr[1].classList.remove('search-error--on');
+        })();
+};
 
 async function request(url, options = {}) {
     try {
@@ -161,3 +188,23 @@ request('./json/got.json').then((data) => {
     });
 });
 
+// Modal starts here
+
+closeModalX.onclick = function () {
+    modal.classList.remove("visible-transition");
+    modal.classList.add("hidden-transition");
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.classList.remove("visible-transition");
+        modal.classList.add("hidden-transition");
+    }
+};
+
+window.onresize = function () {
+    if (window.innerWidth > 599) {
+        modal.classList.remove("visible-transition");
+        modal.classList.add("hidden-transition");
+    }
+};
